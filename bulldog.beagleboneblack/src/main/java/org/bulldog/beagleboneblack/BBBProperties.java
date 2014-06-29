22b03be531b770159df9d08269e19a5d4050d6ba
Datenheld <Datenheld@gmail.com>
2014-06-29 18:41:36 +0200

    Removed unecessary directory

--- bulldog.beagleboneblack/src/main/java/org/bulldog/beagleboneblack/BBBProperties.java
+++
@@ -1,7 +0,0 @@
-package org.bulldog.beagleboneblack;
-
-public class BBBProperties {
-	public static final String DOGTAG = "beagleboneblack.dogtag";
-	public static final String HDMI_ENABLED = "beagleboneblack.hdmi.enabled";
-	public static final String EMMC_ENABLED = "beagleboneblack.emmc.enabled";
-}
