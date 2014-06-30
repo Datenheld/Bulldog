ffe3a49a677cb7c5dea6cd192eb82beb077a5bbe
Datenheld <Datenheld@gmail.com>
2014-06-30 21:59:54 +0200

    Adding distribution zips

--- bulldog.devices/src/main/java/org/bulldog/devices/lcd/HD44780.java
+++ bulldog.devices/src/main/java/org/bulldog/devices/lcd/HD44780.java
@@ -1,7 +1,20 @@
 package org.bulldog.devices.lcd;
 
+import org.bulldog.core.gpio.DigitalOutput;
+
 public class HD44780 {
 
-
+	public HD44780(DigitalOutput d0, 
+				   DigitalOutput d1, 
+				   DigitalOutput d2, 
+				   DigitalOutput d3, 
+				   DigitalOutput d4, 
+				   DigitalOutput d5,
+				   DigitalOutput d6,
+				   DigitalOutput d7, 
+				   DigitalOutput d8) {
+		
+		
+	}
 
 }
---
+++ bulldog.examples/src/main/java/org/bulldog/examples/LcdExample.java
@@ -0,0 +1,18 @@
+package org.bulldog.examples;
+
+import java.io.IOException;
+
+import org.bulldog.core.platform.Board;
+import org.bulldog.core.platform.Platform;
+
+public class LcdExample {
+
+    public static void main(String[] args) throws IOException
+    {
+    	//Grab the platform the application is running on
+    	Board board = Platform.createBoard();
+
+  
+    }
+	
+}
