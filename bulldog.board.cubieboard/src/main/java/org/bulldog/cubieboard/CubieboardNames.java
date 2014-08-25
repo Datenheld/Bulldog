dcb74188870c2d5642348eb4e1d06e3b5d253bec
Eugene Schava <eschava@gmail.com>
2014-08-25 03:36:53 +0300

    added draft of cubieboard support

---
+++ bulldog.board.cubieboard/src/main/java/org/bulldog/cubieboard/CubieboardNames.java
@@ -0,0 +1,26 @@
+package org.bulldog.cubieboard;
+
+public class CubieboardNames {
+
+    // PB
+    public static final String PB18 = "PB18";
+
+    // PG
+	public static final String PG0 = "PG0";
+	public static final String PG1 = "PG1";
+	public static final String PG2 = "PG2";
+//	public static final String PG3 = "PG3";
+	public static final String PG4 = "PG4";
+	public static final String PG5 = "PG5";
+	public static final String PG6 = "PG6";
+	public static final String PG7 = "PG7";
+	public static final String PG8 = "PG8";
+	public static final String PG9 = "PG9";
+	public static final String PG10 = "PG10";
+	public static final String PG11 = "PG10";
+
+    // PH
+    public static final String PH14 = "PH14";
+    public static final String PH15 = "PH15";
+    public static final String PH20 = "PH20";
+}
