90e38d636943467a93ad54036f88ca309ff9c920
Eugene Schava <eschava@gmail.com>
2014-09-02 14:03:01 +0300

    Cubieboard: fast GPIO read/write

--- bulldog.board.cubieboard/src/main/java/org/bulldog/cubieboard/pinfeatures/CubieboardDigitalInput.java
+++ bulldog.board.cubieboard/src/main/java/org/bulldog/cubieboard/pinfeatures/CubieboardDigitalInput.java
@@ -1,6 +1,8 @@
 package org.bulldog.cubieboard.pinfeatures;
 
+import org.bulldog.core.Signal;
 import org.bulldog.core.pinfeatures.Pin;
+import org.bulldog.core.pinfeatures.PinFeatureConfiguration;
 import org.bulldog.core.pinfeatures.event.InterruptListener;
 import org.bulldog.cubieboard.CubieboardPin;
 import org.bulldog.linux.io.LinuxEpollListener;
@@ -9,13 +11,32 @@ import org.bulldog.linux.sysfs.SysFsPin;
 
 public class CubieboardDigitalInput extends LinuxDigitalInput implements LinuxEpollListener {
 
-	public CubieboardDigitalInput(Pin pin) {
+    private final CubieboardGpioMemory gpioMemory;
+    private final int pinIndex;
+    private final int portIndex;
+
+    public CubieboardDigitalInput(Pin pin, CubieboardGpioMemory gpioMemory, int portIndex) {
 		super(pin);
-	}
+
+        this.gpioMemory = gpioMemory;
+        this.pinIndex = pin.getIndexOnPort();
+        this.portIndex = portIndex;
+    }
 
     @Override
     protected SysFsPin createSysFsPin(Pin pin) {
         return new CubieboardSysFsPin(pin.getAddress(), ((CubieboardPin)pin).getFsName(), ((CubieboardPin)pin).isInterrupt());
+    }
+
+    @Override
+    public void setup(PinFeatureConfiguration configuration) {
+        super.setup(configuration);
+        gpioMemory.setPinDirection(portIndex, pinIndex, 0);
+    }
+
+    @Override
+    public Signal read() {
+        return Signal.fromNumericValue(gpioMemory.getPinValue(portIndex, pinIndex));
     }
 
     @Override
